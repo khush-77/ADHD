52ae83127b14209e5a3229b6ba225ff536c72756
khush-77 <khushchahal32@gmail.com>
2025-04-03 20:07:59 +0530

    adhd

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/dhruvbalchandani/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/dhruvbalchandani/ADHD"
+export "FLUTTER_APPLICATION_PATH=/Users/dhruvbalchandani/untitled folder 2/adhd_symptom_tracker/adhd_tracker"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
